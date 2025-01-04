3caf0e7e55b32b7369e78bd7550ba0d70647d3ce
Patrick Selin <patrick.selin@gmail.com>
2025-01-04 14:36:59 +0200

    fix: ci workflow new schema

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use(morganMiddleware);
 app.use("/api1", healthCheckRoutes);
 app.use("/api1", testRoutes);
 app.use("/api1/customers", customerRoutes);
-app.use("/", healthCheckRoutes);
+// app.use("/", healthCheckRoutes);
 
 // middleware
 app.use(unknownEndpoint);
