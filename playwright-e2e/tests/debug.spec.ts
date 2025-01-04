a215b9b25f767291f7145f86d9d8f4e884c46f2a
Patrick Selin <patrick.selin@gmail.com>
2025-01-04 16:46:19 +0200

    fix: ci workflow test db

--- playwright-e2e/tests/debug.spec.ts
+++ playwright-e2e/tests/debug.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("basic page load test", async ({ page }) => {
-  await page.goto("http://BRAKElocalhost:80/home");
+  await page.goto("http://localhost:80/");
 
   await expect(page.locator("h1")).toHaveText("Home", {
     timeout: 10000,
--- playwright-e2e/tests/fetch-data-to-app.spec.ts
+++ playwright-e2e/tests/fetch-data-to-app.spec.ts
@@ -2,18 +2,24 @@ import { test, expect } from "@playwright/test";
 
 test.describe("App data fetching with backend", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:80/home");
+    await page.goto("http://localhost:80/");
   });
 
-  test("should display the first fetched test data from the server/db in the <main>-tag", async ({ page }) => {
-    await expect(page.locator("h2")).toHaveText("TestData", { timeout: 10000 });
+  test("should display the first fetched test data from the server/db in the <main>-tag", async ({
+    page,
+  }) => {
+    await expect(page.locator("h1")).toHaveText("Home", { timeout: 10000 });
 
     const firstItem = page.locator("main li").first();
     await expect(firstItem).toHaveCount(1, { timeout: 10000 });
 
-    await expect(firstItem.locator("h3")).toHaveText("Test Word 1", { timeout: 10000 });
-    await expect(firstItem.locator("p")).toHaveText("Important", { timeout: 10000 });
+    await expect(firstItem.locator("h3")).toHaveText("Test Word 1", {
+      timeout: 10000,
+    });
+    await expect(firstItem.locator("p")).toHaveText("Important", {
+      timeout: 10000,
+    });
 
-    await page.screenshot({ path: 'screenshot.png' });
+    await page.screenshot({ path: "screenshot.png" });
   });
 });
