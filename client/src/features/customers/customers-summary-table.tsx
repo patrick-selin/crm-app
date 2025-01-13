49e21dd9d528462f032d5ee69a5504004f626da7
Patrick Selin <patrick.selin@gmail.com>
2025-01-13 18:51:17 +0200

    feat: UI | add tabs for tables, add customer -modal

---
+++ client/src/features/customers/customers-summary-table.tsx
@@ -0,0 +1,55 @@
+// features/customers/customers-summary-table.tsx
+import { Table } from "@mantine/core";
+import { useCustomersSummary } from "./customers-queries";
+
+const CustomersSummaryTable = () => {
+  const { data: customersSummary, isLoading, error } = useCustomersSummary();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching customers.</p>;
+
+  const rows = customersSummary?.map((customer) => (
+    <Table.Tr key={customer.customerId}>
+      <Table.Td>
+        {customer.firstName} {customer.lastName}
+      </Table.Td>
+      <Table.Td>{customer.email}</Table.Td>
+      <Table.Td>
+        {customer.lastOrderDate && customer.lastOrderDate !== "No orders"
+          ? new Date(customer.lastOrderDate).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })
+          : "N/A"}
+      </Table.Td>
+      <Table.Td>{customer.numOfOrders}</Table.Td>
+      <Table.Td>{customer.totalSpent.toFixed(2)}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <div>
+      <h2>Customers</h2>
+      {/* <div>SORT, FILTERs, SEARCH by name</div> */}
+      <div>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Email</Table.Th>
+              <Table.Th>Last Order</Table.Th>
+              {/* Payment status: "Paid", "Pending", "Overdue". */}
+              <Table.Th>Number of Orders</Table.Th>
+              <Table.Th>Total Spent</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </div>
+      {/* <p>PAGINATION numbers </p> */}
+    </div>
+  );
+};
+
+export default CustomersSummaryTable;
---
+++ client/src/features/customers/customers-table.tsx
@@ -0,0 +1,7 @@
+// features/customers/customers-table.tsx
+
+const CustomersTable = () => {
+  return <div>hello customer table</div>;
+};
+
+export default CustomersTable;
