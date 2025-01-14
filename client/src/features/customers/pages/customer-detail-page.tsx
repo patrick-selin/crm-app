30e635b614c9fe0c5e44a1f770049e254609bf36
Patrick Selin <patrick.selin@gmail.com>
2025-01-14 15:41:37 +0200

    customer-detail-page, fetch customer data and customer orders

--- client/src/features/customers/pages/customer-detail-page.tsx
+++ client/src/features/customers/pages/customer-detail-page.tsx
@@ -1,18 +1,35 @@
 // features/customers/customer-detail.tsx
-
+import { useParams } from "react-router";
 import CustomerInfo from "../components/customer-info";
 import CustomerOrders from "../components/customer-orders";
-import EditCustomerModal from "../compnents/edit-customer-modal";
-import DeleteCustomerButton from "../compnents/delete-customer-button";
-import { Title } from "@mantine/core";
+import EditCustomerModal from "../components/edit-customer-modal";
+import DeleteCustomerButton from "../components/delete-customer-button";
+import { Title, Group, Loader } from "@mantine/core";
+
+import { useCustomer, useCustomerOrders } from "../api/customers-queries";
 
 const CustomerDetail = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { data: customer, isLoading: isCustomerLoading } = useCustomer(id!);
+  const { data: orders, isLoading: isOrdersLoading } = useCustomerOrders(id!);
+
+
+  if (isCustomerLoading || isOrdersLoading) return <Loader />;
+  if (!customer) return <p>Customer not found.</p>;
+
+
   return (
     <div>
       {/* muista back-button */}
-      <Title order={1}>Customer Page</Title>
-      <CustomerInfo />
-      <CustomerOrders />
+      <Title order={1}>Customer Page {id}</Title>
+      <Title order={2}>{customer.firstName} {customer.lastName}</Title>
+      <CustomerInfo customer={customer} />
+      <CustomerOrders orders={orders || []} />
+      <Group>
+        <EditCustomerModal />
+        <DeleteCustomerButton />
+      </Group>
     </div>
   );
 };
