817fa6dbc1bdce76b7b7468e07e50949e81d065b
Patrick Selin <patrick.selin@gmail.com>
2025-01-17 07:56:39 +0200

    auth: user schemas

---
+++ server/src/schemas/user-schemas.ts
@@ -0,0 +1,56 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  userId: z.string().uuid(),
+  username: z.string().min(3).max(100),
+  email: z
+    .string()
+    .email({ message: "Invalid email address" })
+    .max(100, { message: "Email must not exceed 100 characters" }),
+  passwordHash: z.string(),
+  role: z.enum(["admin", "user"]),
+  firstName: z
+    .string()
+    .min(2, { message: "First name is required" })
+    .max(50, { message: "First name must not exceed 50 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "Last name is required" })
+    .max(50, { message: "Last name must not exceed 50 characters" }),
+  phone: z
+    .string()
+    .min(8, { message: "Phone number is required, min 8" })
+    .max(15, { message: "Phone number must not exceed 15 characters" }),
+  address: z
+    .string()
+    .min(2, { message: "Address is required,, min 2" })
+    .max(100, { message: "Address must not exceed 100 characters" }),
+  city: z
+    .string()
+    .min(1, { message: "City is required" })
+    .max(50, { message: "City must not exceed 50 characters" }),
+  postalCode: z
+    .string()
+    .min(5, { message: "Postal code is required, min 5" })
+    .max(6, { message: "Postal code must not exceed 6 characters" }),
+  country: z
+    .string()
+    .min(1, { message: "Country is required" })
+    .max(50, { message: "Country must not exceed 50 characters" }),
+  createdAt: z.date(),
+  updatedAt: z.date().nullable(),
+});
+
+export const RegisterSchema = UserSchema.omit({
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
+  passwordHash: true,
+}).extend({
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
