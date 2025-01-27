aed1d84be21102be92aa5fc683d049f20e308e7c
Patrick Selin <patrick.selin@gmail.com>
2025-01-27 19:18:55 +0200

    tests: GET /customers controller, listCustomers

--- server/src/api/customers/customer-controller.unit.test.ts
+++ server/src/api/customers/customer-controller.unit.test.ts
@@ -1,9 +1,12 @@
 // src/api/customers/customer-controller.unit.test.ts
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import * as customerController from "./customer-controller";
 import * as customerService from "./customer-service";
 import {
   setupControllerTest,
+  createMockCustomer,
 } from "../../tests/test-helpers";
+import { NotFoundError } from "../../utils/errors/app-errors";
 import { Request, Response } from "express";
 
 vi.mock("./customer-service");
@@ -23,5 +26,56 @@ describe("Customer Controller Unit Tests", () => {
   });
 
   describe.only("listCustomers", () => {
-// todo
+    it("should return a list of customers", async () => {
+      const mockCustomers = [
+        createMockCustomer(),
+        createMockCustomer(),
+        createMockCustomer(),
+      ];
+      mockCustomerService.getCustomers.mockResolvedValueOnce({
+        total: 3,
+        page: 1,
+        limit: 10,
+        data: mockCustomers,
+      });
+
+      req.query = { page: "1", limit: "10" };
+
+      await customerController.listCustomers(
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(mockCustomerService.getCustomers).toHaveBeenCalledWith({
+        search: undefined,
+        sort: undefined,
+        page: 1,
+        limit: 10,
+        filters: {},
+      });
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        total: 3,
+        page: 1,
+        limit: 10,
+        data: mockCustomers,
+      });
+    });
+
+    it("should pass errors to next", async () => {
+      const error = new Error("Service Error");
+      req.query = { page: "1", limit: "10" };
+
+      mockCustomerService.getCustomers.mockRejectedValueOnce(error);
+
+      await customerController.listCustomers(
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
 });
