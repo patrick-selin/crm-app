245d0e11c1d1f475314e2a830a69edda956bdb3b
Patrick Selin <patrick.selin@gmail.com>
2025-02-04 15:53:03 +0200

    fix: add types for BulkActionControls

--- client/src/features/orders/components/bulk-action-controls.tsx
+++ client/src/features/orders/components/bulk-action-controls.tsx
@@ -1,14 +1,35 @@
 import { Button, Modal, Text, Group } from "@mantine/core";
 
-const BulkActionsControls = ({ selectedOrders, setSelectedOrders, modalOpened, open, close }) => {
+interface BulkActionsControlsProps {
+  selectedOrders: string[];
+  setSelectedOrders: React.Dispatch<React.SetStateAction<string[]>>;
+  modalOpened: boolean;
+  open: () => void;
+  close: () => void;
+}
+
+const BulkActionsControls: React.FC<BulkActionsControlsProps> = ({
+  selectedOrders,
+  setSelectedOrders,
+  modalOpened,
+  open,
+  close,
+}) => {
   return (
     <>
-      <Button mt="md" color="red" disabled={!selectedOrders.length} onClick={open}>
+      <Button
+        mt="md"
+        color="red"
+        disabled={!selectedOrders.length}
+        onClick={open}
+      >
         Bulk Cancel Orders
       </Button>
 
       <Modal opened={modalOpened} onClose={close} title="Confirm Bulk Action">
-        <Text>Are you sure you want to cancel {selectedOrders.length} orders?</Text>
+        <Text>
+          Are you sure you want to cancel {selectedOrders.length} orders?
+        </Text>
         <Group mt="md">
           <Button
             color="red"
